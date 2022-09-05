fdd4575bda0f5336c5216a2671bebdd2d44d9707
BrianX <qfxiongbin@163.com>
2022-09-05 17:51:51 +0800

    update : notification interface java doc

--- design-pattern/src/main/java/com/brian/dp/apialert/service/Notification.java
+++ design-pattern/src/main/java/com/brian/dp/apialert/service/Notification.java
@@ -2,5 +2,15 @@ package com.brian.dp.apialert.service;
 
 public interface Notification {
 
+    /**
+     * alert notify
+    * @param level:
+    	 * @param message:
+    *
+    * @return void
+    * @author BrianX
+    * @description TODO
+    * @date 2022/9/5 17:50
+    */
     void notify(Enum level,String message);
 }
